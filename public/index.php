08a7c28b172c9d9b08af83033d2863abea01eaca
kevin-illu <kevinillu97@gmail.com>
2023-12-12 00:24:53 -0600

    add(DB): DB class connection

--- public/index.php
+++ public/index.php
@@ -1,10 +1,11 @@
 <?php
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+
 use Slim\Factory\AppFactory;
 
 
-require __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
--- src/System/DB.php
+++ src/System/DB.php
@@ -1,9 +1,24 @@
 <?php
+namespace App\System;
 
 use Doctrine\DBAL\DriverManager as DriverManager;
+use Doctrine\DBAL\Query\QueryBuilder;
 
 class DB {
-  private $db;
+  private $queryBuilder;
   private $conn;
   private $connectionParameters;
+
+  private function __construct(Config $config) {
+    $this->connectionParameters = $config->getDbConfig();
+
+    $this->conn = DriverManager::getConnection
+    ($this->connectionParameters);
+
+    $this->queryBuilder = $this->conn->createQueryBuilder();
+  }
+
+  public function getQueryBuilder(): QueryBuilder {
+    return $this->queryBuilder;
+  }
 }
