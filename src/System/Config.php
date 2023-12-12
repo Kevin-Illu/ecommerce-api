909811f59bdaf1750969d4c6096f8766cbbaba96
kevin-illu <kevinillu97@gmail.com>
2023-12-12 15:53:12 -0600

    fix(ENV): use enviroment variables instead of hard code secrets

--- src/System/Config.php
+++ src/System/Config.php
@@ -1,15 +1,23 @@
 <?php
 namespace App\System;
 
+require __DIR__.'/../../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__."/../../");
+$dotenv->load();
+
+
 class Config {
   private $dbSettings;
   private $errorSettings;
 
   public function __construct() {
-    $host = getenv('DB_HOST');
-    $db   = getenv('DB_DATABASE');
-    $user = getenv('DB_USERNAME');
-    $pass = getenv('DB_PASSWORD');
+    $host = $_ENV['DB_HOST'];
+    $db   = $_ENV['DB_DATABASE'];
+    $user = $_ENV['DB_USERNAME'];
+    $pass = $_ENV['DB_PASSWORD'];
 
 
     $this->dbSettings = [
